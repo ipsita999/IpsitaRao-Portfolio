d72401d463b99d4ea6bd28e6f76bdfd98467ec2f
Ipsita Rao <ipsita.rao@gmail.com>
2020-03-22 14:05:14 -0400

    heroku change

--- server.js
+++ server.js
@@ -63,6 +63,8 @@ router.post('/send', (req, res) => {
 const app = express()
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
