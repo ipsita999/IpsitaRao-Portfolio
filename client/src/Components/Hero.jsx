71215cdf566a8849498834955d85c383c1b96551
Ipsita Rao <ipsita.rao@gmail.com>
2021-08-26 03:09:13 -0400

    notfication changes react way

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import TextLoop from "react-text-loop";
 import '../styles/Hero.css'
 import Nav from './Nav'
-import UA from '../../public/push-worker'
+// import UA from '../../public/index.html'
 
 
 const Hero = () => {
     
 
-    sendNotif = () => {
-        {
-            UA.then(sdk => {
-                    sdk.register().then(console.log('you are registered'))
-            })
-        }
+    // sendNotif = () => {
+    //     {
+    //         UA.then(sdk => {
+    //                 sdk.register().then(console.log('you are registered'))
+    //         })
+    //     }
 
-    }
+    // }
 
-    UA.then(sdk => {
-        sdk.plugins.load('html-prompt', 'https://aswpsdkus.com/notify/v1/ua-html-prompt.min.js', {
-            appearDelay: 5000, 
-            disappearDelay: 17000
-        })
-            .then(plugin => plugin.prompt())
-    })
+    // UA.then(sdk => {
+    //     sdk.plugins.load('html-prompt', 'https://aswpsdkus.com/notify/v1/ua-html-prompt.min.js', {
+    //         appearDelay: 5000, 
+    //         disappearDelay: 17000
+    //     })
+    //         .then(plugin => plugin.prompt())
+    // })
 
 
     
@@ -37,10 +37,10 @@ const Hero = () => {
             <div className='header'>
             I am 
                <p>  [  <TextLoop children={[" a web developer", "a food fanatic   ", "a dog lover", "   an innovator  ","an avid reader   "]} />  ]</p>
-               <button className='regButton' onClick ={sendNotif} > Subscribe!
-                        {/* <a href="/registration-page.html" className='web'>Subscribe!</a> */}
-                    </button>
-
+               {/* <button className='regButton' onClick ={sendNotif} > Subscribe! */}
+                     <div className ='regButton'> <a href="/registration-page.html" className='web'>Subscribe!</a>
+                    {/* </button> */}
+                    </div>
             </div>
        
 
